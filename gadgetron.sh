5f0187aa9324d9099404b5bdf4791a3e1f7f0aa6
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-04-18 10:44:40 -0400

    Obviate datalad

--- gadgetron.sh
+++ gadgetron.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 set -euf -o pipefail
-#cd $(dirname "$0")/..
+cd $(dirname "$0")/..
 project_dir=/project/mscamras/gadgetron
 get_port="import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()"
+TMPDIR=/scratch/
 
-for dat in $(find datasets -name "*.dat" -type l); do
+i=0
+for dat in $(find datasets -name "*.dat" -type f | grep -v .git); do
     outdir=$(dirname $dat)
     outfile=$(basename $dat .dat).h5
     cd $outdir
@@ -19,14 +21,15 @@ for dat in $(find datasets -name "*.dat" -type l); do
         else
             script=siemens_recon.sh
         fi
-        datalad run -i "*" -o "*" -m "reconstructing ${outdir}" \
-        "timeout --signal=KILL 3h singularity run --cleanenv --writable-tmpfs -B \$TMPDIR:/var/run -B \$TMPDIR:/tmp -B \$TMPDIR -B \$TMPDIR:/var/log/supervisor -B \$PWD:/opt/data -B $project_dir --pwd /opt/data ~/simg/ubuntu_1404_cuda75_latest.sif $project_dir/code/$script $(basename $dat) $port1 $port2 $port3" || true
+        #datalad run -i "*" -o "*" -m "reconstructing ${outdir}" \
+        #"timeout --signal=KILL 3h \
+        singularity run --cleanenv --writable-tmpfs -B $TMPDIR:/var/run -B $TMPDIR:/tmp -B $TMPDIR -B $TMPDIR:/var/log/supervisor -B $PWD:/opt/data -B $project_dir --pwd /opt/data ~/simg/ubuntu_1404_cuda75_latest.sif $project_dir/code/$script $(basename $dat) $port1 $port2 $port3 || true 
         mv out.h5 $outfile || true
         rm core.* || true
-        rm noise.h5 || true
+        rm noise.h5 || true 
         rm data.h5 || true
-        cd $project_dir
-        datalad save -r -m "reconstructed ${outdir}" || true
+        #datalad save -r -m "reconstructed ${outdir}" || true
+        ((++i))
     fi
     cd $project_dir
 done
