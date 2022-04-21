ad3668e4d2233559c1f02f19f224b8fb31f4e7f2
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-04-21 15:43:09 -0400

    Edit h5_to_nifti wrapper
    Do not use singularity. Iterate over .h5 files instead of .dat.

--- h5_to_nifti.sh
+++ h5_to_nifti.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 set -euf -o pipefail
-cd $(dirname "$0")/../datasets
+cd /project/mscamras/gadgetron
 
-for dat in $(find . -name "*.dat" -type l); do
-    h5=$(dirname $dat)/$(basename $dat .dat).h5
-    if [ ! -e $h5.nii.gz ]; then
+for h5 in $(find $(pwd) -name "*.h5" -type f | grep -v .git); do
+    #h5=$(dirname $dat)/$(basename $dat .dat).h5
+    out_nifti=$(dirname $h5)/$(basename $h5 .h5).nii.gz # TODO: change output directory to parent after QC
+    
+    if [ ! -e $out_nifti ]; then
         fn=$(basename $h5)
         dir=$(dirname $h5)
         mid=$(echo $fn | grep -Eo "MID[0-9]+" | grep -Eo "[0-9]+")
-        # fid=$(echo $fn | grep -Eo "FID[0-9]+" | grep -Eo "[0-9]+")
-        modality=$(echo $fn | grep -Eo "[A-Z_]+\.dat\.h5")
+        modality=$(echo $fn | grep -Eo "[A-Z_]+\.h5")
         other_fn=$(ls $dir | grep -Eo "meas_MID[0-9]+_FID[0-9]+${modality}" | sort | uniq | grep -v $fn)
         other_mid=$(echo $other_fn | grep -Eo "MID[0-9]+" | grep -Eo "[0-9]+")
-        # other_fid=$(echo $other_fn | grep -Eo "FID[0-9]+" | grep -Eo "[0-9]+")
         if [ "$mid" -gt "$other_mid" ]; then
             # echo "$h5 must be scan 2 since numbers in filename greater than other raw data file: ${other_fn}"
             suffix="a"
@@ -22,7 +22,9 @@ for dat in $(find . -name "*.dat" -type l); do
             suffix=""
         fi
         just_modality=$(basename $(basename $(echo $modality | cut -d"_" -f2,3,4) .h5) .dat)
-        reference_nifti=../../../Data/$(echo $dir | grep -Eo [0-9]+-[0-9]+)/$(echo $dir | grep -Eo [A-Za-z]+)/NIFTI/${just_modality}${suffix}.nii.gz
-        singularity exec --cleanenv ~/simg/neuropythy_latest.sif python ../code/h5_to_nifti.py $h5 $reference_nifti $h5.nii.gz
+        reference_nifti=/project/mscamras/Data/$(echo $dir | grep -Eo [0-9]+-[0-9]+)/$(echo $dir | grep -Eo [0-9]+-[0-9]+-[A-Za-z]+/ | grep -Eo [A-Za-z]+/)NIFTI/${just_modality}_ND${suffix}.nii.gz
+        #bsub -J h5nifti_"$i" -o ../logs/h5_to_nifti.log -e ../logs/h5_to_nifti.log 
+        # singularity exec -B /project --cleanenv ~/simg/neuropythy_latest.sif
+        python code/try.py $h5 $reference_nifti $out_nifti 
     fi
 done
